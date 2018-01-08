9598f18e02c0c966cd79365989d35f841cb40a74
blazpie <blazej.pierzak@gmail.com>
2018-01-08 14:21:39 +0100

    Set up project properities

---
+++ src/main/java/com/codecool/blazpie/spaceshipflotilla/SpaceshipflotillaApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.blazpie.spaceshipflotilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshipflotillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshipflotillaApplication.class, args);
+	}
+}
